3fa6f486b8c51a8efa1f9dad4d9d47f84821c8ac
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2023-07-10 18:03:21 +0000

    Less hackish hack to get the right Ormolu

--- flake.nix
+++ flake.nix
@@ -34,16 +34,18 @@
             ## https://github.com/cachix/pre-commit-hooks.nix/issues/255
             ## for more information.
 
-            ## NOTE: We want Ormolu to be exactly the one provided by HLS.
-            ## However, `pre-commit-hooks.nix` expects as an `ormolu` tool a
-            ## derivation containing a file `bin/ormolu`, which HLS's derivation
-            ## does not provide. We therefore build a fake one that immediately
-            ## executes to the runtime version of `ormolu` provided by HLS.
-            ormolu = pkgs.writeShellApplication {
-              name = "ormolu";
-              runtimeInputs = [ hpkgs.haskell-language-server ];
-              text = ''exec ormolu "$@"'';
-            };
+            ## NOTE: We want Ormolu to be exactly the one provided by HLS, but
+            ## this Ormolu is actually a dependency of `hls-ormolu-plugin`,
+            ## itself a dependency of HLS. We therefore define a function
+            ## `getDeps` that gathers direct Haskell build dependencies and we
+            ## use it to go get our Ormolu.
+            ormolu = let
+              getDeps = p:
+                builtins.listToAttrs (map (dependency:
+                  pkgs.lib.nameValuePair dependency.pname dependency)
+                  p.passthru.getBuildInputs.haskellBuildInputs);
+            in (getDeps (getDeps
+              hpkgs.haskell-language-server).hls-ormolu-plugin).ormolu.bin;
           };
         };
       in {
@@ -79,8 +81,10 @@
           };
 
           default = pkgs.mkShell {
-            ## NOTE: haskell-language-server provides ormolu, so there's no
-            ## need to add it here.
+            ## NOTE: `haskell-language-server` provides Ormolu, so there is no
+            ## need to add it here. If it did not, we would have to resort to a
+            ## trick such as the one used in `pre-commit-hooks`'s configuration
+            ## above.
             buildInputs = buildInputs
               ++ (with hpkgs; [ haskell-language-server hpack hlint ]);
             inherit (pre-commit) shellHook;
